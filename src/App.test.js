b676690a4052b545ca1d189d1ef89c4935b5f22f
Dennis Padiernos <depadiernos@gmail.com>
2019-10-10 21:34:32 -0500

    add integration test to make sure all values increment and ball and strike count resets. need to implement fireEvents to increment.

--- src/App.test.js
+++ src/App.test.js
@@ -5,14 +5,38 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
 afterEach(rtl.cleanup);
-const mockData = {
-  ballCount: 3,
-  strikeCount: 2,
-  foulCount: 4,
-  hitCount: 10
-};
 
-test("Render the app", () => {
+test("ballCount increments", () => {
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByText(/baseball count dashboard/i);
+  expect(element).toBeVisible(1);
+});
+
+test("ballCount resets at 4", () => {
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByText(/ball/i);
+  expect(element).toHaveTextContent(/[0-3]/);
+});
+
+test("strikeCount increments", () => {
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByText(/baseball count dashboard/i);
+  expect(element).toBeVisible(1);
+});
+
+test("strikeCount resets at 3", () => {
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByText(/strike/i);
+  expect(element).toHaveTextContent(/[0-2]/);
+});
+
+test("foulCount increments", () => {
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByText(/baseball count dashboard/i);
+  expect(element).toBeVisible(1);
+});
+
+test("hitCount increments", () => {
   const wrapper = rtl.render(<App />);
   const element = wrapper.getByText(/baseball count dashboard/i);
   expect(element).toBeVisible(1);
