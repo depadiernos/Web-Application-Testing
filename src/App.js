5c725e9d88d11db44d3e2b385ebeddedb6aa7b99
Dennis Padiernos <depadiernos@gmail.com>
2019-10-10 20:22:29 -0500

    implement logic to increment the count for each button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard";
 
 function App() {
+  const [data, setData]=useState({
+    ballCount: 0,
+    strikeCount: 0,
+    foulCount: 0,
+    hitCount: 0
+  })
+
+const updateCount = (name, count)=>{
+  setData({...data, [name]: count+1})
+}
+
   return (
     <div className="App">
-  
+      <Display data={data} />
+      <Dashboard data={data} handleClick={updateCount}/>
     </div>
   );
 }
