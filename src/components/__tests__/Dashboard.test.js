97f0053481b2caf762fe4a7c797d711847e9f57d
Dennis Padiernos <depadiernos@gmail.com>
2019-10-10 21:01:53 -0500

    add test to make sure buttons render with correct button text

--- src/components/__tests__/Dashboard.test.js
+++ src/components/__tests__/Dashboard.test.js
@@ -1,15 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Dashboard from "./Dashboard";
-
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "../Dashboard";
 
 afterEach(rtl.cleanup)
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Dashboard />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("Render Ball Count", () => {
+
+  const wrapper = rtl.render(<Dashboard/>);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/ball/i);
 });
 
+test("Render Strike Count", () => {
+
+  const wrapper = rtl.render(<Dashboard />);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/strike/i);
+});
+
+test("Render Foul Count", () => {
+
+  const wrapper = rtl.render(<Dashboard />);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/foul/i);
+});
+
+test("Render Hit Count", () => {
+
+  const wrapper = rtl.render(<Dashboard />);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/hit/i);
+});
