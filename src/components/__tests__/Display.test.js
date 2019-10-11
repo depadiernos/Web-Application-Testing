158ac6039646a5d70d8b46fabae50d441ea34d61
Dennis Padiernos <depadiernos@gmail.com>
2019-10-10 21:01:19 -0500

    add test to make sure display renders the correct counts

--- src/components/__tests__/Display.test.js
+++ src/components/__tests__/Display.test.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Display from '../Display';
 
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 afterEach(rtl.cleanup);
+const mockData = {
+  ballCount: 3,
+  strikeCount: 2,
+  foulCount: 4,
+  hitCount: 10
+}
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Display />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("Render Ball Count", () => {
+
+  const wrapper = rtl.render(<Display data={mockData}/>);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/ball/i);
+  expect(element).toHaveTextContent("Ball: 3");
+});
+
+test("Render Strike Count", () => {
+
+  const wrapper = rtl.render(<Display data={mockData}/>);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/strike/i);
+  expect(element).toHaveTextContent("Strike: 2");
+});
+
+test("Render Foul Count", () => {
+
+  const wrapper = rtl.render(<Display data={mockData}/>);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/foul/i);
+  expect(element).toHaveTextContent("Foul: 4");
+});
+
+test("Render Hit Count", () => {
+
+  const wrapper = rtl.render(<Display data={mockData}/>);
+  // using a regular expression instead of a string allows our
+  // query to be much more flexible. for example, if the text becomes
+  // all uppercase, we don't want our test to break
+  const element = wrapper.getByText(/hit/i);
+  expect(element).toHaveTextContent("Hit: 10");
 });
