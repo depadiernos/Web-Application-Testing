201342807305b4d51dd08edadc4b727279c7404f
Dennis Padiernos <depadiernos@gmail.com>
2019-10-11 03:51:04 -0500

    implement fireEvent and assert value based on how many times the fireEvent was fired.

--- src/App.test.js
+++ src/App.test.js
@@ -8,36 +8,55 @@ afterEach(rtl.cleanup);
 
 test("ballCount increments", () => {
   const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/baseball count dashboard/i);
-  expect(element).toBeVisible(1);
+  const display = wrapper.getByText(/ball:\W/i);
+  const button = wrapper.getByText("Ball");
+  rtl.fireEvent.click(button)
+  expect(display).toHaveTextContent("Ball: 1");
 });
 
 test("ballCount resets at 4", () => {
   const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/ball/i);
-  expect(element).toHaveTextContent(/[0-3]/);
+  const display = wrapper.getByText(/ball:\W/i);
+  const button = wrapper.getByText("Ball");
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  expect(display).toHaveTextContent(/[0-3]/);
 });
 
 test("strikeCount increments", () => {
   const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/baseball count dashboard/i);
-  expect(element).toBeVisible(1);
+  const display = wrapper.getByText(/strike:\W/i);
+  const button = wrapper.getByText("Strike");
+  rtl.fireEvent.click(button)
+  expect(display).toHaveTextContent("Strike: 1");
 });
 
 test("strikeCount resets at 3", () => {
   const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/strike/i);
-  expect(element).toHaveTextContent(/[0-2]/);
+  const display = wrapper.getByText(/strike:\W/i);
+  const button = wrapper.getByText("Strike");
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  expect(display).toHaveTextContent(/[0-2]/);
 });
 
 test("foulCount increments", () => {
   const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/baseball count dashboard/i);
-  expect(element).toBeVisible(1);
+  const display = wrapper.getByText(/foul:\W/i);
+  const button = wrapper.getByText("Foul");
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  expect(display).toHaveTextContent("Foul: 2");
 });
 
 test("hitCount increments", () => {
   const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/baseball count dashboard/i);
-  expect(element).toBeVisible(1);
+  const display = wrapper.getByText(/hit:\W/i);
+  const button = wrapper.getByText("Hit");
+  rtl.fireEvent.click(button)
+  rtl.fireEvent.click(button)
+  expect(display).toHaveTextContent("Hit: 2");
 });
