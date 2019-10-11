f57590e97cac89e95fcdc285b5262f9f3e53e975
Dennis Padiernos <depadiernos@gmail.com>
2019-10-10 21:03:05 -0500

    start integration test if click increments value.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+const mockData = {
+  ballCount: 3,
+  strikeCount: 2,
+  foulCount: 4,
+  hitCount: 10
+}
+
+
+test("Render the app", () => {
+  // render our React app into an in-memory DOM so we can test against it
+  const wrapper = rtl.render(<App />);
+
+  // element is now our dom element that we can test against
+  const element = wrapper.getByText(/baseball count dashboard/i);
+
+  // test will fail if element is not visible to our robot eyes
+  expect(element).toBeVisible(1);
 });
