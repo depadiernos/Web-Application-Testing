7ef3e91ed9a6c01bc1a7cec4a25fd52009815901
Dennis Padiernos <depadiernos@gmail.com>
2019-10-10 21:34:51 -0500

    implement strike and ball reset limit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import Display from "./components/Display";
 import Dashboard from "./components/Dashboard";
 
 function App() {
-  const [data, setData]=useState({
+  const [data, setData] = useState({
     ballCount: 0,
     strikeCount: 0,
     foulCount: 0,
     hitCount: 0
-  })
+  });
 
-const updateCount = (name, count)=>{
-  setData({...data, [name]: count+1})
-}
+  const updateCount = (name, count) => {
+    if (name === "ballCount" && count === 3) {
+      count = 0;
+    } else if (name === "strikeCount" && count === 2) {
+      count = 0;
+    } else {
+      count += 1;
+    }
+    setData({ ...data, [name]: count });
+  };
 
   return (
     <div className="App">
       <h1>Baseball Count Dashboard</h1>
       <Display data={data} />
-      <Dashboard data={data} handleClick={updateCount}/>
+      <Dashboard data={data} handleClick={updateCount} />
     </div>
   );
 }
